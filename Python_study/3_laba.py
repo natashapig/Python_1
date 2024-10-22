b1ed17608f8989190b524ed9e8af278aacff8c0d
natashapig <krylova.230805@gmail.com>
2024-10-22 23:04:47 +0300

    complete

---
+++ Python_study/3_laba.py
@@ -0,0 +1,17 @@
+f = open('3.txt', 'r', encoding='utf-8')
+d={
+    '0':'ноль',
+    '2':'два',
+    '4':'четыре',
+    '6':'шесть',
+    '8':'восемь',
+}
+s = f.readline().split()
+while s!=[]:
+    for i in range(len(s)):
+        if s[i].isnumeric() and int(s[i][0])%2!=0:
+            print()
+            for j in s[i]:
+                if int(j)%2==0: print(d[j],end='')
+                else: print(j, end='')
+    s = f.readline().split()
