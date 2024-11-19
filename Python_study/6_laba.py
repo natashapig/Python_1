3ff54b6cc421ccb1a74d7246c712e1ee4c9dea66
natashapig <krylova.230805@gmail.com>
2024-11-19 22:47:14 +0300

    changed

--- Python_study/6_laba.py
+++ Python_study/6_laba.py
@@ -16,10 +16,11 @@ def alg(n):
             a.append(i)
     return a
 def func(n):
-    a = str([ch for ch in range(1,n,2)])
+    a = [str(ch) for ch in range(1,n,2)]
     matches=[]
     for el in a:
         match = re.match(r"^[13579]\d*[13579]?$", el)
+        print(el)
         if match: matches.append(int(el))
     return matches
 n=input('Введите целое число n: ')
