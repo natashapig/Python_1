b94dce4ae3d3a47c9fc01f1f25c3b51906223d3e
natashapig <krylova.230805@gmail.com>
2024-11-19 22:01:01 +0300

    changed

--- Python_study/5_laba.py
+++ Python_study/5_laba.py
@@ -1,46 +1,35 @@
 '''Задана рекуррентная функция. Область определения функции – натуральные числа. Написать 
 программу сравнительного вычисления данной функции рекурсивно и итерационно. Определить границы 
-применимости рекурсивного и итерационного подхода. Результаты сравнительного исследования 
+применимости рекурсивного и итеративного подхода. Результаты сравнительного исследования 
 времени вычисления представить в табличной форме. Обязательное требование – минимизация времени 
 выполнения и объема памяти. F(1)=1,F(n)= (-1)^n*(F(n–1) + (n + 1)! / (2n)!), при n > 1'''
-from timeit import Timer
-from functools import partial
-import math
-n=input('Введите натуральное число n > 1: ')
+from math import factorial
+import timeit
+fact1, fact2 = 2, 2
+n=input('Введите натуральное число n > 1 и n < 996: ')
 while True:
-    if n.isnumeric():
-        n=int(n)
-        if n>1: break
-    n = input('Вы неправильно ввели n. Введите натуральное число n > 1: ')
-def fact(n):
-    factorial = 1
-    while n > 1:
-        factorial *= n
-        n -= 1
-    return factorial
+    try: n = int(n)
+    except: n = input('Вы неправильно ввели n. Введите натуральное число n > 1 и n < 996: ')
+    else: 
+        if n>1 and n<1001: break
+        else: n = input('Вы неправильно ввели n. Введите натуральное число n > 1 и n < 996: ')
+def st1(n):
+    if n%2==0: return 1
+    else: return -1
 def recursive_f(n):
-  if n == 1: return 1
-  else:
-    try: return (-1)**n * (recursive_f(n - 1) + fact(n + 1) / fact(2*n))
-    except: return 0
-def iterative_f(n):
-    f = 1
-    for i in range(2, n + 1): f = (-1)**i * (f + fact(i + 1) / fact(2*i))
+    if n == 1: return 1
+    else: return st1(n) * (recursive_f(n-1) + factorial(n+1) / factorial(2*n))    
+def iterative_f(n, fact1,fact2):
+    f=1
+    for i in range(2,n+1):
+        fact1 *=(i+1) 
+        fact2 *= (i*2) * ((i*2)-1)
+        f = st1(i) * (f + fact1/fact2)               
     return f
 def compare_methods(n):
     print(f"n: {n}")
-    recursive_result = recursive_f(n)
-    if recursive_result==0:
-        print('Невозможно вычислить эту функцию рекурсивно.')
-    else:
-        recursive_time = Timer(partial(recursive_f,n)).timeit(number=1000)
-        print(f"Рекурсивный результат: {recursive_result}")
-        print(f"Время рекурсивного вычисления: {recursive_time:.6f} секунд")
-    iterative_result = iterative_f(n)
-    iterative_time = Timer(partial(recursive_f,n)).timeit(number=1000)
-    print(f"Итеративный результат: {iterative_result}")
-    print(f"Время итеративного вычисления: {iterative_time:.6f} секунд")
+    a = timeit.timeit('recursive_f(n)', globals = globals(), number =1)
+    print(f'Рекурсивный результат: {recursive_f(n)} \nВремя рекурсивного вычисления: {a}')
+    a = timeit.timeit('iterative_f(n, fact1, fact2)', globals = globals(), number =1)
+    print(f'Итерационный результат: {iterative_f(n, fact1,fact2)} \nВремя итерационного вычисления: {a}')
 compare_methods(n)
-print("Границы применимости:")
-print("Рекурсивный подход: подходит для небольших значений n, так как время вычисления растет экспоненциально.")
-print("Итеративный подход: более эффективен для больших значений n, так как время вычисления линейно зависит от n.")
