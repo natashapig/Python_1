e50ce495548b61264f7dfb06a4995a16aebc5eab
natashapig <krylova.230805@gmail.com>
2024-11-22 14:38:52 +0300

    changed

--- Python_study/6_laba2.py
+++ Python_study/6_laba2.py
@@ -5,31 +5,45 @@
 ограничение на характеристики объектов (которое будет сокращать количество переборов) и целевую 
 функцию для нахождения оптимального  решения.
 Вывести все натуральные числа до n, которые начинаются и заканчиваются нечетной цифрой.
-Найти сумму всех чисел, в которых первая ии последняя цифры совпадают.
+Найти наибольшую сумму цифр среди выведенных чисел, вывести с наибольшим остатком от деления 
+на 3.
 '''
 import timeit
 import re
 
 def alg(n):
-    summ=0
+    tsumm=0
+    msumm=0
+    chislo=0
     a=[]
     for i in range(1,n,2):
         if int(str(i)[0]) % 2 != 0:
             a.append(i)
-            if str(i)[0]==str(i)[-1]:
-                summ+=i
-    print(f"Сумма всех чисел, в которых первая и последняя цифры совпадают: {summ}")
+            for cz in str(i):
+                tsumm+=int(cz)
+            if tsumm%3==2 and msumm<tsumm:
+                    msumm=tsumm
+                    chislo=i
+    print(f"Наибольшая сумма цифр в выведенных числах, которая при делении на 3 дает наибольший остаток(2), находится в числе {chislo}: {msumm}")
     return a
 def func(n):
-    summ=0
+    tsumm=0
+    msumm=0
+    chislo=0
     a = [str(ch) for ch in range(1,n,2)]
     matches=[]
     for el in a:
         match = re.match(r"^[13579]\d*$", el)
         if match:
-            if el[0]==el[-1]: summ+=int(el)
+            ch=[]
+            for cz in el:
+                ch.append(int(cz))
+            tsumm=sum(ch)
+            if tsumm%3==2 and tsumm>msumm:
+                msumm=max(tsumm,msumm)
+                chislo=int(el)
             matches.append(int(el))
-    print(f"Сумма всех чисел, в которых первая и последняя цифры совпадают: {summ}")
+    print(f"Наибольшая сумма цифр в выведенных числах, которая при делении на 3 дает наибольший остаток(2), находится в числе {chislo}: {msumm}")
     return matches
 n=input('Введите целое число n: ')
 while True:
