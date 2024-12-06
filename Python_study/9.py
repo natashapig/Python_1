9117c725573448283de9498663516502af5b6194
natashapig <krylova.230805@gmail.com>
2024-12-06 13:15:34 +0300

    changed

--- Python_study/9.py
+++ Python_study/9.py
@@ -3,7 +3,7 @@ import random
 from tkinter.messagebox import *
 root = Tk()
 root.title('Крестики Нолики')
-root.geometry('300x300')
+root.geometry('300x295+620+180')
 games=[]
 choice = 0
 combinations = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
